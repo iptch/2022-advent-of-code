e8f7d7d0cd64e758e63bde624256307365715471
Manuela Züger <manuela.zueger@ipt.ch>
2022-12-04 06:40:47 +0100

    rename test method

--- mzu/4/test_b.py
+++ mzu/4/test_b.py
@@ -5,7 +5,7 @@ import year2022_day04b as part_b
 
 class Test(unittest.TestCase):
 
-    def test_fully_contains_sections(self):
+    def test_has_overlapping_sections(self):
         self.assertTrue(part_b.has_overlapping_sections([2, 3, 4, 5, 6], [3, 4]))
         self.assertTrue(part_b.has_overlapping_sections([3, 4], [2, 3, 4, 5, 6]))
         self.assertTrue(part_b.has_overlapping_sections([2, 3, 4, 5, 6], [2, 3, 4, 5, 6]))
