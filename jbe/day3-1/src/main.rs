97d91bc0c609d4dd6894a3a49b9567e73526fc67
Jakob Beckmann <jakobbeckmann@pm.me>
2022-12-04 13:57:00 +0100

    feat(jbe): add memory hungry but faster approach to day 3

--- jbe/day3-1/src/main.rs
+++ jbe/day3-1/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 const OFFSET_LOWER: usize = 96;
 const OFFSET_UPPER: usize = 64 - 26;
 
@@ -8,6 +10,12 @@ fn main() {
         .map(|chr| score(chr.unwrap()))
         .sum();
     println!("The sum of duplicate items in the compartments is: {sum}");
+    let sum_set_based: usize = common::lines("day3-1/assets/input.txt")
+        .map(|line| find_duplicate_set_based(&line))
+        .filter(|chr| chr.is_some())
+        .map(|chr| score(chr.unwrap()))
+        .sum();
+    println!("The sum of duplicate items in the compartments is (set based): {sum_set_based}");
 }
 
 
@@ -22,13 +30,13 @@ fn score(ch: char) -> usize {
 
 
 
-// This should be much faster than simple lookup which is worst case O(n^2) worst case with n being
-// the length of the compartment. This uses two partern-defeating quicksorts which are only O(n *
-// log(n)) worst case, followed by a simple traversal in O(n). This means this should be worst case
-// O(n * log(n) + n). In terms of memory, the sorting is done in place, which means we only
-// allocate a single vector per compartment. Since we pipe the reference to this function, this
-// should mean that we only assign each character twice in the entire program, once at read time,
-// and once in this function.
+// This should be much faster than simple lookup which is worst case O(n^2) with n being the length
+// of the compartment. It uses no HashMap in order to optimize memory usage. This uses two
+// partern-defeating quicksorts which are only O(n * log(n)) worst case, followed by a simple
+// traversal in O(n). This means this should be worst case O(n * log(n) + n). In terms of memory,
+// the sorting is done in place, which means we only allocate a single vector per compartment.
+// Since we pipe the reference to this function, this should mean that we only assign each
+// character twice in the entire program, once at read time, and once in this function.
 fn find_duplicate(backpack: &str) -> Option<char> {
     let backpack_len = backpack.len();
     assert!(backpack_len % 2 == 0);
@@ -50,6 +58,24 @@ fn find_duplicate(backpack: &str) -> Option<char> {
             idx1 += 1;
         } else {
             idx2 += 1;
+        }
+    }
+
+    None
+}
+
+
+// This solution is O(n), but it is boring. Moreover, due to the usage of a HashMap, it is more
+// memory hungry than the solution above.
+fn find_duplicate_set_based(backpack: &str) -> Option<char> {
+    let backpack_len = backpack.len();
+    assert!(backpack_len % 2 == 0);
+    let (comptmnt1, comptmnt2) = backpack.split_at(backpack_len / 2);
+    let comptmnt2_set: HashSet<char> = comptmnt2.chars().collect();
+
+    for ch in comptmnt1.chars() {
+        if comptmnt2_set.contains(&ch) {
+            return Some(ch)
         }
     }
 
--- jbe/day3-2/src/main.rs
+++ jbe/day3-2/src/main.rs
@@ -1,15 +1,24 @@
 #![feature(iter_array_chunks)]
+use std::collections::HashSet;
 
 const OFFSET_LOWER: usize = 96;
 const OFFSET_UPPER: usize = 64 - 26;
 
 fn main() {
-    let sum: usize = common::lines("day3-2/assets/input.txt").array_chunks()
+    let sum: usize = common::lines("day3-2/assets/input.txt")
+        .array_chunks()
         .map(|[bp1, bp2, bp3]| find_duplicate(&bp1, &bp2, &bp3))
         .filter(|chr| chr.is_some())
         .map(|chr| score(chr.unwrap()))
         .sum();
     println!("The sum of badges is: {sum}");
+    let sum_set_based: usize = common::lines("day3-2/assets/input.txt")
+        .array_chunks()
+        .map(|[bp1, bp2, bp3]| find_duplicate_set_based(&bp1, &bp2, &bp3))
+        .filter(|chr| chr.is_some())
+        .map(|chr| score(chr.unwrap()))
+        .sum();
+    println!("The sum of badges is (set_based): {sum_set_based}");
 }
 
 fn score(ch: char) -> usize {
@@ -23,7 +32,10 @@ fn score(ch: char) -> usize {
 // This should be much faster than simple lookup which is worst case O(n^2) worst case with n being
 // the length of the compartment. This uses several partern-defeating quicksorts which are only O(n
 // * log(n)) worst case, followed by a simple traversal in O(n). This means this should be worst
-// case O(n * log(n) + n).
+// case O(n * log(n) + n). In terms of memory, the sorting is done in place, which means we only
+// allocate a single vector per compartment. Since we pipe the reference to this function, this
+// should mean that we only assign each character twice in the entire program, once at read time,
+// and once in this function.
 fn find_duplicate(backpack1: &str, backpack2: &str, backpack3: &str) -> Option<char> {
     let mut state = vec![
         (get_sorted(backpack1), 0usize, backpack1.len()),
@@ -52,6 +64,22 @@ fn find_duplicate(backpack1: &str, backpack2: &str, backpack3: &str) -> Option<c
     }
 
     None
+}
+
+// This solution is O(n), but it is boring. Moreover, due to the usage of HashMaps, it is more
+// memory hungry than the solution above.
+fn find_duplicate_set_based(backpack1: &str, backpack2: &str, backpack3: &str) -> Option<char> {
+    let backpack1_set: HashSet<char> = backpack1.chars().collect();
+    let backpack2_set: HashSet<char> = backpack2.chars().collect();
+    let backpack3_set: HashSet<char> = backpack3.chars().collect();
+
+    let mut all_chars = backpack1
+        .chars()
+        .chain(backpack2.chars())
+        .chain(backpack3.chars());
+    all_chars.find(|ch| {
+        backpack1_set.contains(ch) && backpack2_set.contains(ch) && backpack3_set.contains(ch)
+    })
 }
 
 // sorts the state based on the smallest current character being observed
