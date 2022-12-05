1d393f484cbe088d9b688a3a1e8800bb5e77be5f
daniel <daniel.steinmann@gmail.com>
2022-12-05 11:07:45 +0100

    way too many lines

---
+++ dst/day6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace aoc2022
+{
+    class day6
+    {
+        public static void Main(string[] args)
+        {
+            string filepath = "sample_input.txt";
+            // string filepath = "input.txt";
+
+            using (StreamReader sr = File.OpenText(filepath))
+            {
+                string? line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                }
+            }
+        }
+    }
+}
