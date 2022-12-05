b62d43c8fc36b02fda999b2aa2ed675e5f2e5afa
daniel <daniel.steinmann@gmail.com>
2022-12-05 09:17:52 +0100

    prepare files

---
+++ dst/day5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace aoc2022
+{
+    class day5
+    {
+        public static void Main(string[] args)
+        {
+            string filepath = "sample_input.txt";
+            // string filepath = "input.txt";
+
+            using (StreamReader sr = File.OpenText(filepath))
+            {
+                string? line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    char[] delimiters = { ',', '-' };
+                    int[] num = line.Split(delimiters).Select(x => int.Parse(x)).ToArray();
+                }
+            }
+        }
+    }
+}
