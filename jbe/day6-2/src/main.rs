4ee19b9ba18fefc24181e893afb2df6667314833
Jakob Beckmann <jakobbeckmann@pm.me>
2022-12-06 08:30:12 +0100

    feat(jbe): add day6 solutions

---
+++ jbe/day6-2/src/main.rs
@@ -0,0 +1,22 @@
+#![feature(array_windows)]
+
+use std::collections::HashSet;
+
+const WINDOW_SIZE: usize = 14;
+
+fn main() {
+    let line = common::lines("day6-2/assets/input.txt").next().unwrap();
+    let chars: Vec<char> = line.chars().collect();
+    let index: usize = chars
+        .array_windows::<WINDOW_SIZE>()
+        .enumerate()
+        .find(|(_, slice)| uniq(*slice))
+        .map(|(idx, _)| idx + WINDOW_SIZE)
+        .unwrap();
+    println!("The first marker appears on position: {index}");
+}
+
+fn uniq(slice: &[char]) -> bool {
+    let mut unique = HashSet::new();
+    slice.iter().all(move |x| unique.insert(x))
+}
