87a48d411ec428855690c8ec22c12c8a3d02425e
Max Riedel <m_riedel_95@gmx.de>
2022-12-06 22:38:47 +0100

    day2-2 completed. haven't started crying.. yet

--- mri/day02-rock-paper-scissor/src/main.rs
+++ mri/day02-rock-paper-scissor/src/main.rs
@@ -2,15 +2,52 @@ use common::read_lines;
 
 fn main() {
     let mut score = 0;
+    let mut score2 = 0;
 
     for line in read_lines("assets/input.txt") {
         let opponent: char = line.chars().nth(0).unwrap();
         let you: char = line.chars().nth(2).unwrap();
 
         score += calc_score(opponent, you);
+        score2 += calc_score_2(opponent, you);
     }
 
     println!("score: {}", score);
+    println!("score 2: {}", score2)
+}
+
+fn calc_score_2(opponent: char, you: char) -> u32 {
+    let mut score = 0;
+    match you {
+        'X' => { // lose
+            match opponent {
+                'A' => score += 3,
+                'B' => score += 1,
+                'C' => score += 2,
+                _ => eprintln!("Unknown shape: {}", opponent),
+            };
+        },
+        'Y' => { // draw
+            score += 3;
+            match opponent {
+                'A' => score += 1,
+                'B' => score += 2,
+                'C' => score += 3,
+                _ => eprintln!("Unknown shape: {}", opponent),
+            }
+        }
+        'Z' => { // win
+            score += 6;
+            match opponent {
+                'A' => score += 2,
+                'B' => score += 3,
+                'C' => score += 1,
+                _ => eprintln!("Unknown shape: {}", opponent),
+            }
+        }
+        _ => eprintln!("Unknown shape: {}", you),
+    };
+    score
 }
 
 fn calc_score(opponent: char, you: char) -> u32 {
