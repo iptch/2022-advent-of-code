237fb9babb4f0beea21fbbe6bb9939129a5a6450
Max Riedel <m_riedel_95@gmx.de>
2022-12-06 21:30:22 +0100

    day1 completed. that was easy. only lagging 5 days behind. rust is fun.

--- mri/day01-calorie-counting/src/main.rs
+++ mri/day01-calorie-counting/src/main.rs
@@ -1,14 +1,16 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use common::read_lines;
 
 fn main() {
     
     let mut current_elf_calories = 0;
     let mut max_elf_calories = 0;
 
+    let mut aggregated_calories = Vec::new();
+
     for line in read_lines("assets/input.txt") {
         if line.is_empty() {
+            aggregated_calories.push(current_elf_calories);
+
             if current_elf_calories > max_elf_calories {
                 max_elf_calories = current_elf_calories;
             }
@@ -22,14 +24,12 @@ fn main() {
         max_elf_calories = current_elf_calories;
     }
 
-    println!("Maximum calories carried by an elf: {}", max_elf_calories);
+    aggregated_calories.sort();
+    aggregated_calories.reverse();
+    let total_calories_top3 = aggregated_calories[0] + aggregated_calories[1] + aggregated_calories[2];
 
-    // The output is wrapped in a Result to allow matching on errors
-    // Returns an Iterator to the Reader of the lines of the file.
-    fn read_lines<P>(filename: P) -> impl Iterator<Item=String> where P: AsRef<Path>, {
-        let file = File::open(filename).unwrap();
-        let reader = io::BufReader::new(file);
-        reader.lines().map(|x| x.unwrap())
-    }
+    println!("Maximum calories carried by an elf: {}", max_elf_calories);
+    println!("Total amount caried by top 3 elves: {}", total_calories_top3);
+
 
 }
