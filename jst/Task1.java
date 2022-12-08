796ed3053b3f4f06d789db1de36a0bf2c4c06bcc
Jennifer Studer <jst@ipt.ch>
2022-12-08 19:42:08 +0100

    improve template

--- jst/Task1.java
+++ jst/Task1.java
@@ -1,11 +1,25 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Task1 {
     public static void main(String[] args) {
-        String input = getInput();
+//        String[] input = getInput();
+        String[] input = getTestInput();
 
 
     }
 
-    private static String getInput(){
-        return "";
+    private static String[] getTestInput() {
+        String input = "";
+        return input.split("\n");
+    }
+
+    private static String[] getInput() {
+        try (InputStream resourceAsStream = Task1.class.getResourceAsStream("input.txt");){
+            String input = new String(resourceAsStream.readAllBytes());
+            return input.split("\r\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
