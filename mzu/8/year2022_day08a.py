2a59b14d533ce5bf997c84d47e1fcb75af26f902
Manuela Züger <manuela.zueger@ipt.ch>
2022-12-08 07:37:06 +0100

    improve naming

--- mzu/8/year2022_day08a.py
+++ mzu/8/year2022_day08a.py
@@ -4,49 +4,49 @@ DAY = '8'
 PART = 'a'
 
 
-def tree_is_at_edge_of_forest(forest_size_x, forest_size_y, x, y):
-    return x == 0 or y == 0 or x == forest_size_x - 1 or y == forest_size_y - 1
+def tree_is_at_edge_of_forest(forest_size_rows, forest_size_cols, row_idx, col_idx):
+    return row_idx == 0 or col_idx == 0 or row_idx == forest_size_rows - 1 or col_idx == forest_size_cols - 1
 
 
-def tree_is_visible_from_left(forest, x, y):
+def tree_is_visible_from_left(forest, row_idx, col_idx):
     result = True
-    for y_left in range(y - 1, -1, -1):
-        if forest[x][y_left] >= forest[x][y]:
+    for col_idx_left_of_tree in range(col_idx - 1, -1, -1):
+        if forest[row_idx][col_idx_left_of_tree] >= forest[row_idx][col_idx]:
             result = False
             break
     return result
 
 
-def tree_is_visible_from_right(forest, x, y):
+def tree_is_visible_from_right(forest, row_idx, col_idx):
     result = True
-    for y_right in range(y + 1, len(forest[0]), +1):
-        if forest[x][y_right] >= forest[x][y]:
+    for col_idx_right_of_tree in range(col_idx + 1, len(forest[0]), +1):
+        if forest[row_idx][col_idx_right_of_tree] >= forest[row_idx][col_idx]:
             result = False
             break
     return result
 
 
-def tree_is_visible_from_top(forest, x, y):
+def tree_is_visible_from_top(forest, row_idx, col_idx):
     result = True
-    for x_top in range(x - 1, -1, -1):
-        if forest[x_top][y] >= forest[x][y]:
+    for row_idx_above_tree in range(row_idx - 1, -1, -1):
+        if forest[row_idx_above_tree][col_idx] >= forest[row_idx][col_idx]:
             result = False
             break
     return result
 
 
-def tree_is_visible_from_bottom(forest, x, y):
+def tree_is_visible_from_bottom(forest, row_idx, col_idx):
     result = True
-    for x_bottom in range(x + 1, len(forest[0]), +1):
-        if forest[x_bottom][y] >= forest[x][y]:
+    for row_idx_below_tree in range(row_idx + 1, len(forest[0]), +1):
+        if forest[row_idx_below_tree][col_idx] >= forest[row_idx][col_idx]:
             result = False
             break
     return result
 
 
-def check_if_tree_is_visible(forest, x, y):
-    return tree_is_visible_from_left(forest, x, y) or tree_is_visible_from_right(forest, x, y) \
-           or tree_is_visible_from_top(forest, x, y) or tree_is_visible_from_bottom(forest, x, y)
+def check_if_tree_is_visible(forest, row_idx, col_idx):
+    return tree_is_visible_from_left(forest, row_idx, col_idx) or tree_is_visible_from_right(forest, row_idx, col_idx) \
+           or tree_is_visible_from_top(forest, row_idx, col_idx) or tree_is_visible_from_bottom(forest, row_idx, col_idx)
 
 
 def main():
@@ -57,14 +57,14 @@ def main():
     forest = [[int(tree_size) for tree_size in row] for row in lines]
     visible_trees = [[0 for _ in row] for row in forest]
 
-    forest_size_x = len(lines)
-    forest_size_y = len(lines[0])
-    for x in range(forest_size_x):
-        for y in range(forest_size_y):
-            if tree_is_at_edge_of_forest(forest_size_x, forest_size_y, x, y):
-                visible_trees[x][y] = True
+    forest_size_rows = len(lines)
+    forest_size_cols = len(lines[0])
+    for row_idx in range(forest_size_rows):
+        for col_idx in range(forest_size_cols):
+            if tree_is_at_edge_of_forest(forest_size_rows, forest_size_cols, row_idx, col_idx):
+                visible_trees[row_idx][col_idx] = True
             else:
-                visible_trees[x][y] = check_if_tree_is_visible(forest, x, y)
+                visible_trees[row_idx][col_idx] = check_if_tree_is_visible(forest, row_idx, col_idx)
 
     number_of_visible_trees = len([tree for column in visible_trees for tree in column if tree])
     print(f'There are a total of {str(number_of_visible_trees)} visible trees in the forest.')
--- mzu/8/year2022_day08b.py
+++ mzu/8/year2022_day08b.py
@@ -4,49 +4,49 @@ DAY = '8'
 PART = 'b'
 
 
-def tree_is_not_at_edge_of_forest(forest_size_x, forest_size_y, x, y):
-    return not (x == 0 or y == 0 or x == forest_size_x - 1 or y == forest_size_y - 1)
+def tree_is_not_at_edge_of_forest(forest_size_rows, forest_size_cols, row_idx, col_idx):
+    return not (row_idx == 0 or col_idx == 0 or row_idx == forest_size_rows - 1 or col_idx == forest_size_cols - 1)
 
 
-def scenic_score_left(forest, x, y):
+def scenic_score_left(forest, row_idx, col_idx):
     score = 0
-    for y_left in range(y - 1, -1, -1):
+    for col_idx_left_of_tree in range(col_idx - 1, -1, -1):
         score += 1
-        if not forest[x][y_left] < forest[x][y]:
+        if not forest[row_idx][col_idx_left_of_tree] < forest[row_idx][col_idx]:
             break
     return score
 
 
-def scenic_score_right(forest, x, y):
+def scenic_score_right(forest, row_idx, col_idx):
     score = 0
-    for y_right in range(y + 1, len(forest[0]), +1):
+    for col_idx_right_of_tree in range(col_idx + 1, len(forest[0]), +1):
         score += 1
-        if not forest[x][y_right] < forest[x][y]:
+        if not forest[row_idx][col_idx_right_of_tree] < forest[row_idx][col_idx]:
             break
     return score
 
 
-def scenic_score_top(forest, x, y):
+def scenic_score_top(forest, row_idx, col_idx):
     score = 0
-    for x_top in range(x - 1, -1, -1):
+    for row_idx_above_tree in range(row_idx - 1, -1, -1):
         score += 1
-        if not forest[x_top][y] < forest[x][y]:
+        if not forest[row_idx_above_tree][col_idx] < forest[row_idx][col_idx]:
             break
     return score
 
 
-def scenic_score_bottom(forest, x, y):
+def scenic_score_bottom(forest, row_idx, col_idx):
     score = 0
-    for x_bottom in range(x + 1, len(forest[0]), +1):
+    for row_idx_below_tree in range(row_idx + 1, len(forest[0]), +1):
         score += 1
-        if not forest[x_bottom][y] < forest[x][y]:
+        if not forest[row_idx_below_tree][col_idx] < forest[row_idx][col_idx]:
             break
     return score
 
 
-def calculate_scenic_score_of_tree(forest, x, y):
-    return scenic_score_left(forest, x, y) * scenic_score_right(forest, x, y) \
-           * scenic_score_top(forest, x, y) * scenic_score_bottom(forest, x, y)
+def calculate_scenic_score_of_tree(forest, row_idx, col_idx):
+    return scenic_score_left(forest, row_idx, col_idx) * scenic_score_right(forest, row_idx, col_idx) \
+           * scenic_score_top(forest, row_idx, col_idx) * scenic_score_bottom(forest, row_idx, col_idx)
 
 
 def main():
@@ -57,12 +57,12 @@ def main():
     forest = [[int(tree_size) for tree_size in row] for row in lines]
     scenic_scores = [[0 for _ in row] for row in forest]
 
-    forest_size_x = len(lines)
-    forest_size_y = len(lines[0])
-    for x in range(forest_size_x):
-        for y in range(forest_size_y):
-            if tree_is_not_at_edge_of_forest(forest_size_x, forest_size_y, x, y):
-                scenic_scores[x][y] = calculate_scenic_score_of_tree(forest, x, y)
+    forest_size_rows = len(lines)
+    forest_size_cols = len(lines[0])
+    for row_idx in range(forest_size_rows):
+        for col_idx in range(forest_size_cols):
+            if tree_is_not_at_edge_of_forest(forest_size_rows, forest_size_cols, row_idx, col_idx):
+                scenic_scores[row_idx][col_idx] = calculate_scenic_score_of_tree(forest, row_idx, col_idx)
 
     max_scenic_score = max([tree for column in scenic_scores for tree in column if tree])
     print(f'The tree with the best view has a scenic score of {str(max_scenic_score)}.')
