5fe31ef95b144397b45dde017d3e6d3ef83f625f
matthaeusheer <matthaeusheer@gmail.com>
2022-12-11 13:00:10 +0100

    made loading func more versatile

--- mhr/common.py
+++ mhr/common.py
@@ -4,6 +4,9 @@ import os
 PROJECT_ROOT_PATH = os.path.dirname((__file__))
 
 
-def load_lines(day: int, file_name: str = 'input.txt') -> list[str]:
+def load_lines(day: int, file_name: str = 'input.txt', skip_empty_lines: bool = False) -> list[str]:
     with open(f'{PROJECT_ROOT_PATH}/day{day}/{file_name}') as infile:
-        return [line.strip() for line in infile.readlines()]
+        lines = [line.strip() for line in infile.readlines()]
+        if skip_empty_lines:
+            lines = [line for line in lines if line != '']
+        return lines
