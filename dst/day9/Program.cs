40241256cf161031dd44917a2aaf162274d0f953
daniel <daniel.steinmann@gmail.com>
2022-12-11 13:43:33 +0100

    d9part2

--- dst/day9/Program.cs
+++ dst/day9/Program.cs
@@ -8,144 +8,102 @@ namespace aoc2022
         public static void Main(string[] args)
         {
             // string filepath = "sample_input.txt";
-            string filepath = "sample_input_2.txt";
-            // string filepath = "input.txt";
-            List<(char, int)> parsedCommands = new();
-            ParseCommands(filepath, parsedCommands);
-            List<char> commands = new();
-            ExpandCommands(parsedCommands, commands);
+            // const int ROPE_LENGTH = 2;
+            // string filepath = "sample_input_2.txt";
+            const int ROPE_LENGTH = 10;
+            string filepath = "input.txt";
 
-            // Initialize Head
-            (int x, int y) head;
-            (int x, int y) tail;
+            List<(char, int)> commands = new();
+            ParseCommands(filepath, commands);
 
-            // Record History
-            List<(int x, int y)> headHistory = new();
-            List<(int x, int y)> tailHistory = new();
-
-            // Starting position
-            head = (0, 0);
-            headHistory.Add(head);
-            tail = (0, 0);
-            tailHistory.Add(tail);
-
-
-            // Part 1
-            MoveHeadForAllCommands(commands, headHistory);
-
-            foreach ((int x, int y) item in headHistory)
+            // Initialize Rope
+            List<(int x, int y)> rope = new();
+            for (int i = 0; i < ROPE_LENGTH; i++)
             {
-                (int x, int y) oldTail = tailHistory.Last();
-                (int x, int y) newTail = updateTail(item, oldTail);
-                tailHistory.Add(newTail);
+                rope.Add((0, 0));
             }
 
-            int uniquePositions = tailHistory.Distinct().Count();
-            Console.WriteLine("Unique Positions: {0}", uniquePositions);
+            // Initialize Tail Positions
+            List<(int x, int y)> tailPositions = new();
+            tailPositions.Add((0, 0));
 
-            // Part 2
-
-            List<(int x, int y)> tail0 = new() { (0, 0) };
-            List<(int x, int y)> tail1 = new() { (0, 0) };
-            List<(int x, int y)> tail2 = new() { (0, 0) };
-            List<(int x, int y)> tail3 = new() { (0, 0) };
-            List<(int x, int y)> tail4 = new() { (0, 0) };
-            List<(int x, int y)> tail5 = new() { (0, 0) };
-            List<(int x, int y)> tail6 = new() { (0, 0) };
-            List<(int x, int y)> tail7 = new() { (0, 0) };
-            List<(int x, int y)> tail8 = new() { (0, 0) };
-            List<(int x, int y)> tail9 = new() { (0, 0) };
-
-            foreach ((int x, int y) item in headHistory)
+            foreach ((char direction, int steps) command in commands)
             {
-                update2(tail0, item);
-                update2(tail1, tail0.Last());
-                update2(tail2, tail1.Last());
-                update2(tail3, tail2.Last());
-                update2(tail4, tail3.Last());
-                update2(tail5, tail4.Last());
-                update2(tail6, tail5.Last());
-                update2(tail7, tail6.Last());
-                update2(tail8, tail7.Last());
-                update2(tail9, tail8.Last());
+                for (int i = 0; i < command.steps; i++)
+                {
+                    (int x, int y) newHeadPosition = GetNewHeadPosition(rope[0], command.direction);
+                    rope[0] = newHeadPosition;
+
+                    for (int j = 1; j < ROPE_LENGTH; j++)
+                    {
+                        (int x, int y) newKnotPosition = GetNewKnotPosition(rope[j - 1], rope[j]);
+                        rope[j] = newKnotPosition;
+                    }
+
+                    tailPositions.Add(rope[ROPE_LENGTH - 1]);
+                }
             }
 
-            int uniquePositionsPart2 = tail9.Distinct().Count();
-            Console.WriteLine("Part 2 Unique Positions: {0}", uniquePositionsPart2);
-        }
-        private static void update2(List<(int x, int y)> tailHistory, (int x, int y) item)
-        {
-            (int x, int y) oldTail = tailHistory.Last();
-            (int x, int y) newTail = updateTail(item, oldTail);
-            tailHistory.Add(newTail);
+            Console.WriteLine("Unique tail positions: {0}", tailPositions.Distinct().Count());
         }
 
-        private static (int x, int y) updateTail((int x, int y) head, (int x, int y) tail)
+        private static (int x, int y) GetNewKnotPosition((int x, int y) frontKnot, (int x, int y) backKnot)
         {
-            (int x, int y) newTail = new();
-            int dx = head.x - tail.x;
-            int dy = head.y - tail.y;
+            (int x, int y) newBackKnot = new();
+            int dx = frontKnot.x - backKnot.x;
+            int dy = frontKnot.y - backKnot.y;
 
             if (Math.Abs(dx) <= 1 && Math.Abs(dy) <= 1)
             {
-                newTail = tail;
+                newBackKnot = backKnot;
             }
             else if (dx == 0 || dy == 0)
             {
                 if (dx == 0)
                 {
-                    newTail.x = tail.x;
-                    if (dy > 0) { newTail.y = tail.y + 1; }
-                    if (dy < 0) { newTail.y = tail.y - 1; }
+                    newBackKnot.x = backKnot.x;
+                    if (dy > 0) { newBackKnot.y = backKnot.y + 1; }
+                    if (dy < 0) { newBackKnot.y = backKnot.y - 1; }
                 }
                 else if (dy == 0)
                 {
-                    newTail.y = tail.y;
-                    if (dx > 0) { newTail.x = tail.x + 1; }
-                    if (dx < 0) { newTail.x = tail.x - 1; }
+                    newBackKnot.y = backKnot.y;
+                    if (dx > 0) { newBackKnot.x = backKnot.x + 1; }
+                    if (dx < 0) { newBackKnot.x = backKnot.x - 1; }
                 }
             }
             else
             {
-                if (dx == 2)
+                if (dx >= 2)
                 {
-                    newTail.x = tail.x + 1;
-                    if (dy > 0) { newTail.y = tail.y + 1; }
-                    if (dy < 0) { newTail.y = tail.y - 1; }
+                    newBackKnot.x = backKnot.x + 1;
+                    if (dy > 0) { newBackKnot.y = backKnot.y + 1; }
+                    if (dy < 0) { newBackKnot.y = backKnot.y - 1; }
                 }
-                else if (dx == -2)
+                else if (dx <= -2)
                 {
-                    newTail.x = tail.x - 1;
-                    if (dy > 0) { newTail.y = tail.y + 1; }
-                    if (dy < 0) { newTail.y = tail.y - 1; }
+                    newBackKnot.x = backKnot.x - 1;
+                    if (dy > 0) { newBackKnot.y = backKnot.y + 1; }
+                    if (dy < 0) { newBackKnot.y = backKnot.y - 1; }
                 }
-                else if (dy == 2)
+                else if (dy >= 2)
                 {
-                    newTail.y = tail.y + 1;
-                    if (dx > 0) { newTail.x = tail.x + 1; }
-                    if (dx < 0) { newTail.x = tail.x - 1; }
+                    newBackKnot.y = backKnot.y + 1;
+                    if (dx > 0) { newBackKnot.x = backKnot.x + 1; }
+                    if (dx < 0) { newBackKnot.x = backKnot.x - 1; }
                 }
-                else if (dy == -2)
+                else if (dy <= -2)
                 {
-                    newTail.y = tail.y - 1;
-                    if (dx > 0) { newTail.x = tail.x + 1; }
-                    if (dx < 0) { newTail.x = tail.x - 1; }
+                    newBackKnot.y = backKnot.y - 1;
+                    if (dx > 0) { newBackKnot.x = backKnot.x + 1; }
+                    if (dx < 0) { newBackKnot.x = backKnot.x - 1; }
                 }
             }
 
-            return newTail;
+            return newBackKnot;
         }
 
-        private static void MoveHeadForAllCommands(List<char> commands, List<(int x, int y)> headHistory)
-        {
-            foreach (char command in commands)
-            {
-                (int x, int y) newPosition = MoveKnot(headHistory.Last(), command);
-                headHistory.Add(newPosition);
-            }
-        }
-
-        private static (int x, int y) MoveKnot((int x, int y) input, char command)
+        private static (int x, int y) GetNewHeadPosition((int x, int y) input, char command)
         {
             (int x, int y) knot = input;
             switch (command)
@@ -157,17 +115,6 @@ namespace aoc2022
             }
 
             return knot;
-        }
-
-        private static void ExpandCommands(List<(char, int)> parsedCommands, List<char> commands)
-        {
-            foreach ((char, int) item in parsedCommands)
-            {
-                for (int i = 0; i < item.Item2; i++)
-                {
-                    commands.Add(item.Item1);
-                }
-            }
         }
 
         private static void ParseCommands(string filepath, List<(char, int)> commands)
