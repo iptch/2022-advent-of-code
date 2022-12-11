83553b321b7dc7948e7d26cf023119b52eeff95b
Manuela Züger <manuela.zueger@ipt.ch>
2022-12-11 16:44:12 +0100

    solutions to day 11

--- mzu/11/test.py
+++ mzu/11/test.py
@@ -6,18 +6,34 @@ import year2022_day11b as part_b
 
 class Test(unittest.TestCase):
 
+    def test_parse_monkey(self):
+        lines = [
+            'Monkey 0:',
+            '  Starting items: 79, 98',
+            '  Operation: new = old * 19',
+            '  Test: divisible by 23',
+            '    If true: throw to monkey 2',
+            '    If false: throw to monkey 3'''
+        ]
+        monkey = part_a.parse_monkey(lines)
+        self.assertListEqual(monkey.items, [79, 98])
+        self.assertEqual(monkey.operation, "old * 19")
+        self.assertEqual(monkey.test_divider, 23)
+        self.assertEqual(monkey.receiver_passed_test, 2)
+        self.assertEqual(monkey.receiver_failed_test, 3)
+
     def test_part_a_with_example_data(self):
         with open('test-data') as file:
-            lines = [line.strip() for line in file.readlines()]
+            lines = [line for line in file.readlines()]
             result = part_a.solve(lines)
-            self.assertEqual(result, 0)
+            self.assertEqual(result, 10605)
             print(result)
 
     def test_part_b_with_example_data(self):
         with open('test-data') as file:
-            lines = [line.strip() for line in file.readlines()]
+            lines = [line for line in file.readlines()]
             result = part_b.solve(lines)
-            self.assertEqual(result, 0)
+            self.assertEqual(result, 2713310158)
             print(result)
 
 
--- mzu/11/year2022_day11a.py
+++ mzu/11/year2022_day11a.py
@@ -1,20 +1,67 @@
+import re
+from dataclasses import dataclass
+
 from aocd import data
 
 DAY = '11'
 PART = 'a'
 
 
+@dataclass
+class Monkey:
+    items: []
+    operation: None
+    test_divider: None
+    receiver_passed_test: None
+    receiver_failed_test: None
+    inspection_counter: 0
+
+
+def parse_monkey(monkey_lines: [str]) -> Monkey:
+    items = [int(item) for item in re.findall(r'[0-9]+', monkey_lines[1])]
+    operation = monkey_lines[2].removeprefix('  Operation: new = ')
+    test_divider = int(re.findall(r'[0-9]+', monkey_lines[3])[0])
+    receiver_passed_test = int(re.findall(r'[0-9]+', monkey_lines[4])[0])
+    receiver_failed_test = int(re.findall(r'[0-9]+', monkey_lines[5])[0])
+    return Monkey(items, operation, test_divider, receiver_passed_test, receiver_failed_test, 0)
+
+
+def parse_input(lines) -> [Monkey]:
+    monkeys = []
+    i = 0
+    while i < len(lines):
+        monkeys.append(parse_monkey(lines[i:i + 6]))
+        i += 7
+    return monkeys
+
+
+def play_round(monkeys):
+    for monkey in monkeys:
+        for _ in range(len(monkey.items)):
+            monkey.inspection_counter += 1
+            old = monkey.items.pop(0)
+            new = eval(monkey.operation)
+            new = new // 3
+            if new % monkey.test_divider == 0:
+                monkeys[monkey.receiver_passed_test].items.append(new)
+            else:
+                monkeys[monkey.receiver_failed_test].items.append(new)
+    return monkeys
+
+
 def solve(lines):
-    result = 0
-    return result
+    monkeys = parse_input(lines)
+    for i in range(20):
+        monkeys = play_round(monkeys)
+
+    monkey_inspection_counters = sorted([monkey.inspection_counter for monkey in monkeys], reverse=True)
+    return monkey_inspection_counters[0] * monkey_inspection_counters[1]
 
 
 def main():
     print(f'Advent of Code 2022 --- Day {DAY} --- Part {PART}')
-
     lines = data.splitlines()
     result = solve(lines)
-
     print(f'{str(result)}')
 
 
