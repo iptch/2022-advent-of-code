bf8c4866ab0f5d2ae87b8442c64509619ecabfad
matthaeusheer <matthaeusheer@gmail.com>
2022-12-23 22:37:10 +0100

    solutions for day 22 part 1

--- mhr/common.py
+++ mhr/common.py
@@ -4,9 +4,16 @@ import os
 PROJECT_ROOT_PATH = os.path.dirname((__file__))
 
 
-def load_lines(day: int, file_name: str = 'input.txt', skip_empty_lines: bool = False) -> list[str]:
+def load_lines(day: int, file_name: str = 'input.txt', skip_empty_lines: bool = False, do_strip: bool = True) -> list[str]:
     with open(f'{PROJECT_ROOT_PATH}/day{day}/{file_name}') as infile:
-        lines = [line.strip() for line in infile.readlines()]
+        lines = infile.readlines()
+        if do_strip:
+            lines = [line.strip() for line in lines]
         if skip_empty_lines:
             lines = [line for line in lines if line != '']
         return lines
+
+
+def print_grid(grid: list[list[str]]) -> None:
+    for line in grid:
+        print(''.join(line))
